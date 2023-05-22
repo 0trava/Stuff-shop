fd8966944f46708e6eed76343818b4d01d1e4f46
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-22 22:09:26 +0300

    add 1:24:54

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,6 +1,11 @@
+import { useGetProductQuery } from 'features/api/apiSlice';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-function SingleProduct() {
+const SingleProduct = () => {
+    const {id} = useParams();
+    const {data} = useGetProductQuery({id})
+
   return (
     <div>SingleProduct</div>
   )
