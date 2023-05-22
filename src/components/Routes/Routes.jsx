4a72262d6d4615e3a7d7fecf1f11f81579fd8e13
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-22 21:52:37 +0300

    add 1:18:36

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
+import {ROUTES} from '../../utils/routes';
+import SingleProduct from 'components/Products/SingleProduct';
 
 
 const AppRoutes = () => (
     
         <Routes>
             <Route index element={<Home />} />
+            <Route path={ROUTES.PRODUCT}  element={<SingleProduct/>}/>
         </Routes>
     );
 
