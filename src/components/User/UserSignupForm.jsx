c9102e3ca7c3f00b22457253fc5ae798df8e884c
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-23 16:25:49 +0300

    add form

--- src/components/User/UserSignupForm.jsx
+++ src/components/User/UserSignupForm.jsx
@@ -4,7 +4,17 @@ import styles from "../../styles/User.module.css";
 
 const UserSignupForm = () => {
   return (
-    <div>
+    <div className={styles.wrapper}>
+        <div className={styles.close}>
+            <svg className="icon">
+                <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#close`}/>
+            </svg>
+        </div>
+
+        <h2 className={styles.title}>Sign Up</h2>
+        <form  className={styles.form}>
+
+        </form>
 
     </div>
   )
