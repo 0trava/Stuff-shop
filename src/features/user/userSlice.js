7d435ad4786d62a5cd02cb9babcd655e215c1cad
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-23 14:55:38 +0300

    add userslice

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,55 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axios from "axios";
+import {BASE_URL} from "../../utils/constants";
+
+// export const getCategories = createAsyncThunk(
+//     "categories/getCategories",
+//     async (_, thunkAPI) => {
+//       try {
+//         const res = await axios(`${BASE_URL}/categories`);
+//         return res.data;
+//       } catch (err) {
+//         console.log(err);
+//         return thunkAPI.rejectWithValue(err);
+//       }
+//     }
+//   );
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState:{
+        currentUser: [],
+        cart: [],
+        isLoading: false,
+    },
+    reducers: {
+        addItemToCart: (state, {payload}) => {
+            let newCart = [...state.cart];
+            const found = state.cart.find(({id}) => id === payload.id)
+
+            if (found) {
+                newCart = newCart.map((item) => {
+                    return item.id === payload.id ? {...item, quantity: payload.quantity || item.quantity +1}
+                    : item;
+                })
+            }
+        }
+        
+    },
+
+    extraReducers:(builder) => {
+        // builder.addCase(getCategories.pending, (state) => {
+        //     state.isLoading = true;
+        // })
+        // builder.addCase(getCategories.fulfilled, (state, action) => {
+        //     state.list = action.payload;
+        //     state.isLoading = false;
+        // })
+        // builder.addCase(getCategories.rejected, (state) => {
+        //     state.isLoading = false;
+        //     console.log("404")
+        // })
+    }
+})
+
+export default userSlice.reducer;
