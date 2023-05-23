27055f82420175c16d883f296327b1e4270e7600
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-23 12:51:54 +0300

    add hook

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import styles from "../../styles/Products.module.css";
+import styles from "../../styles/Product.module.css";
 
-const Product = () => {
+const Product = ({price, title, images}) => {
   return (
     <section className={styles.product}>
         <div className={styles.images}>
@@ -13,8 +13,13 @@ const Product = () => {
                     key={i}
                     className={styles.image}
                     style = {{backgroundImage: `url(${image})`}}
+                    onClick={() => {}}
                     />
             ))}
+        </div>
+        <div className={styles.info}>
+            <h1 className={styles.title}>{title}</h1>
+            <div className={styles.price}>{price}</div>
         </div>
     </section>
   )
