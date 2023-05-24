c2b3cae206e18cf6f9c02daa6a5b8544bcec811e
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-24 14:26:33 +0300

    add 2:21:20

--- src/components/User/UserSignupForm.jsx
+++ src/components/User/UserSignupForm.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
 
 import styles from "../../styles/User.module.css";
+import { useDispatch } from 'react-redux';
+import { createUser } from "../../features/user/userSlice";
 
 const UserSignupForm = ({closeForm}) => {
+    const dispatch = useDispatch();
+
     const [values, setValues ] = useState({
         name: "",
         email: "",
         password: "",
         avatar: "",
     });
+
+
+    // Фунція відправки форми реєстрації
+    const handleSumite = (e) => {
+        e.preventDefault();
+        const isNotEmpty = Object.values(values).some(val => val);
+
+        if (!isNotEmpty) return;
+        dispatch(createUser(values));
+        closeForm();
+        
+    }
 
     const handleChange = ({target: {value, name}}) =>{
         setValues({...values, [name]: value});
@@ -23,13 +39,17 @@ const UserSignupForm = ({closeForm}) => {
         </div>
 
         <h2 className={styles.title}>Sign Up</h2>
-        <form  className={styles.form}>
-         <div className={styles.group}>
-            <input type="email" placeholder='Your email' name="email" value={values.email} autoComplete="off" onChange={handleChange} required/>
-            <input type="name" placeholder='Your name' name="name" value={values.name} autoComplete="off" onChange={handleChange} required/>
-            <input type="password" placeholder='Your password' name="password" value={values.password} autoComplete="off" onChange={ handleChange} required/>
-            <input type="avatar" placeholder='Your avatar' name="avatar" value={values.avatar} autoComplete="off" onChange={handleChange} required/>
-         </div>
+        <form  className={styles.form} onSubmit={handleSumite}>
+         <div className={styles.group}><input type="email" placeholder='Your email' name="email" value={values.email} autoComplete="off" onChange={handleChange} required/></div>
+         <div className={styles.group}><input type="name" placeholder='Your name' name="name" value={values.name} autoComplete="off" onChange={handleChange} required/></div>
+         <div className={styles.group}><input type="password" placeholder='Your password' name="password" value={values.password} autoComplete="off" onChange={ handleChange} required/></div>
+         <div className={styles.group}><input type="avatar" placeholder='Your avatar' name="avatar" value={values.avatar} autoComplete="off" onChange={handleChange} required/></div>
+            
+        
+            
+            
+            
+
 
          <div className={styles.link}>I already have an account</div>
          <button type='submit' className={styles.submit}>Create an account</button>
